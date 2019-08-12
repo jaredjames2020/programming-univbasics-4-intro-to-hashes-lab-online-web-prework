4769509875f7e51ef6451053927c7ff39464eeb0
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-12 23:05:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,9 +36,9 @@ def update_counting_hash(hash, key)
  
    def update_hash(hash, key) 
     if hash.has_key?(key)
-      hash[key] += 1
+      return hash[key] += 1
     else
-      hash[key] = 1
+      return hash[key] = 1
     end
   end
 end
