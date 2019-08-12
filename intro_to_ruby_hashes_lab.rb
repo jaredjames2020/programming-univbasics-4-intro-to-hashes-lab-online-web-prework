36c3622afb5792e6c7a612a053c44bd5345749c4
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-12 22:49:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,22 +33,11 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  update_hash = hash
   
-  
-  if hash[key]
+  if update_hash.has_key?(key)
     hash[key] += 1
   else
     hash[key] = 1
   end
 end
-
-
-a = {:good => 1}
-if a.has_key?(:bad)
-  a[:good] += 1
-  p "one"
-else
-  a[:bad] = 1
-  p "two"
-end
-p a
